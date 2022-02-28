31ab9b6a7e17f7a07b274381f6af135d2ae57aae
Betsy Susana <betsysusanarodriguez@gmail.com>
2022-02-28 06:51:14 -0600

    tres ultimo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,6 @@
 INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Betsy', 'Tapia', 'susy_11@gotmail.com', '2018-06-13');
 INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Brenda', 'Arias', 'brenda.an@gmail.com', '2018-06-13');
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(3, 'Alan', 'Rodriguez', 'alan.bb@gmail.com', '2022-03-13');
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(4, 'cleo', 'romero', 'cleo.an@gmail.com', '2020-08-17');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(3, 'Alan', 'Rodriguez', 'alan.bb@gmail.com', '2020-03-10');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(4, 'cleo', 'romero', 'cleo.an@gmail.com', '2021-08-17');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(5, 'Pepe', 'Campos', 'pepe.C@gmail.com', '2022-05-15');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(6, 'Ana', 'Rosales', 'anita@gmail.com', '2022-01-23');
